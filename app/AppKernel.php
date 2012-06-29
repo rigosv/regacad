b9f81da2786f3ce569912b3d8cca177eb1148017
Rigoberto Reyes <ogir@KaibaCorp>
2012-06-29 16:49:02 -0600

    Agregado el bundle para los datafixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new UES\RegAcadBundle\RegAcadBundle(),
             new UES\jQueryBundle\jQueryBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
